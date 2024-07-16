c1aad91680f9efa920989a8defb6262710e70f37
salihasli <aslisalh9@gmail.com>
2024-07-16 11:46:45 +0300

    Add files via upload

--- pp.py
+++ pp.py
@@ -1,21 +1,45 @@
 import streamlit as st
 
-st.title("Enter (customer) informaiton ")
+# تهيئة حالة تسجيل الدخول
+if 'logged_in' not in st.session_state:
+    st.session_state.logged_in = False
 
-hello=st.text_input(" THE NAME  ")
-phone=st.text_input(" phone number ")
-city=st.selectbox("select city", ["بغداد", "البصرة","نينوى","الانبار","ديالى","كربلاء","بابل","واسط","صلاح الدين","القادسيه","ذي قار","المثنى","ميسان","السليمانية","دهوك","لاربي","كركوك","النجف","الموصل","حلبجة"])
-region= st.text_input("enter the region")
-more=st.text_area("type here for more information")
-file=st.file_uploader("choice file if you want", type=["jpg","png","pdf","docx"])
+# تعريف اليوزر والرمز الصحيح
+user = "admin"
+pas = "132"
 
-if st.button("continue"):
-    st.write("Name", hello)
-    st.write("phone number",phone)
-    st.write("city",city)
-    st.write("region",region)
-    st.write("more informiton",more)
-if file is not None:
-        st.write("File uploaded successfully.")
-        if file.type in ["image/jpeg", "image/png"]:
-            st.image(file, caption="Uploaded Image")
+# صفحة الدخول
+if not st.session_state.logged_in:
+    st.title("Login Page")
+    qq = st.text_input("User Name")
+    ww = st.text_input("Password", type="password")
+    go = st.button("Login")
+    
+    if go:
+        if qq == user and ww == pas:
+            st.session_state.logged_in = True
+            st.success("Login successful")
+        else:
+            st.error("Error: Incorrect user or password")
+
+# صفحة المعلومات بعد تسجيل الدخول
+if st.session_state.logged_in:
+    st.title("Enter Customer Information")
+    hello = st.text_input("THE NAME")
+    phone = st.text_input("Phone Number")
+    city = st.selectbox("Select City", ["بغداد", "البصرة", "نينوى", "الانبار", "ديالى", "كربلاء", "بابل", "واسط", "صلاح الدين", "القادسيه", "ذي قار", "المثنى", "ميسان", "السليمانية", "دهوك", "لاربي", "كركوك", "النجف", "الموصل", "حلبجة"])
+    region = st.text_input("Enter the Region")
+    more = st.text_area("Type here for more information")
+    file = st.file_uploader("Upload your photo", type=["png", "jpg", "jpeg"])
+
+    if st.button("Continue"):
+        st.write("Name:", hello)
+        st.write("Phone Number:", phone)
+        st.write("City:", city)
+        st.write("Region:", region)
+        st.write("More Information:", more)
+        
+    if file:
+        st.image(file, caption="Uploaded Image")
+
+    
