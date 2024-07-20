ca4c7b1c6e9274dd14e1bba7a4b7f0abdb439328
salihasli <aslisalh9@gmail.com>
2024-07-20 12:46:46 +0300

    Add files via upload

--- pp.py
+++ pp.py
@@ -5,24 +5,17 @@ hide_button_style = """
     <style>
     .viewerBadge_container__r5tak,
     .styles_viewerBadge__CvC9N,
-    .viewerBadge_link__qRIco {
+    .viewerBadge_link__qRIco,
+    .viewerBadge_container {
         display: none !important;
     }
     </style>
     <script>
     document.addEventListener("DOMContentLoaded", function() {
-        var elements = document.getElementsByClassName('viewerBadge_container__r5tak');
-        for (var i = 0; i < elements.length; i++) {
-            elements[i].style.display = 'none';
-        }
-        var elements2 = document.getElementsByClassName('styles_viewerBadge__CvC9N');
-        for (var i = 0; i < elements2.length; i++) {
-            elements2[i].style.display = 'none';
-        }
-        var elements3 = document.getElementsByClassName('viewerBadge_link__qRIco');
-        for (var i = 0; i < elements3.length; i++) {
-            elements3[i].style.display = 'none';
-        }
+        var elements = document.querySelectorAll('.viewerBadge_container__r5tak, .styles_viewerBadge__CvC9N, .viewerBadge_link__qRIco, .viewerBadge_container');
+        elements.forEach(function(element) {
+            element.style.display = 'none';
+        });
     });
     </script>
 """
