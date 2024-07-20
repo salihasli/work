cfc917e610d7fa285f123bde0afd058daaf50626
salihasli <aslisalh9@gmail.com>
2024-07-20 12:17:34 +0300

    Add files via upload

--- pp.py
+++ pp.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import json
 
 # إخفاء الزر البرتقالي
 hide_orange_button_style = """
@@ -10,13 +9,3 @@ hide_orange_button_style = """
     </style>
 """
 st.markdown(hide_orange_button_style, unsafe_allow_html=True)
-
-
-# عنوان للصفحة
-st.title('أدخل معلوماتك')
-
-# خانة إدخال النص
-user_input = st.text_input("أدخل معلومة هنا:")
-
-# عرض النص الذي أدخله المستخدم
-st.write("المعلومة التي أدخلتها هي:", user_input)
