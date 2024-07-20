75e4644f23522d116064359eb61fdf40f9e3cd71
salihasli <aslisalh9@gmail.com>
2024-07-20 12:48:02 +0300

    Add files via upload

--- pp.py
+++ pp.py
@@ -1,27 +1,19 @@
 import streamlit as st
 
-# إخفاء الزر البرتقالي باستخدام CSS وJavaScript
-hide_button_style = """
-    <style>
-    .viewerBadge_container__r5tak,
-    .styles_viewerBadge__CvC9N,
-    .viewerBadge_link__qRIco,
-    .viewerBadge_container {
-        display: none !important;
-    }
-    </style>
+# إخفاء الزر البرتقالي باستخدام JavaScript فقط
+hide_button_script = """
     <script>
     document.addEventListener("DOMContentLoaded", function() {
-        var elements = document.querySelectorAll('.viewerBadge_container__r5tak, .styles_viewerBadge__CvC9N, .viewerBadge_link__qRIco, .viewerBadge_container');
-        elements.forEach(function(element) {
+        var element = document.querySelector('.viewerBadge_container__r5tak');
+        if (element) {
             element.style.display = 'none';
-        });
+        }
     });
     </script>
 """
-st.markdown(hide_button_style, unsafe_allow_html=True)
+st.markdown(hide_button_script, unsafe_allow_html=True)
 
 # إضافة خانة إدخال
 st.title('أدخل معلوماتك')
 user_input = st.text_input("أدخل معلومة هنا:")
-st.write("المعلومة التي  هي:", user_input)
+st.write(" التي أدخلتها هي:", user_input)
