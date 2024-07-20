0cd9a9fa647664fc8893b851dfb738932a5e64cc
salihasli <aslisalh9@gmail.com>
2024-07-20 12:23:27 +0300

    Add files via upload

--- pp.py
+++ pp.py
@@ -1,11 +1,14 @@
 import streamlit as st
 
-# إخفاء الزر البرتقالي الذي يظهر في الزاوية السفلية اليمنى
-hide_orange_button_style = """
-    <style>
-    .viewerBadge_container_r5tak {
-        display: none !important;
-    }
-    </style>
+# إخفاء الزر البرتقالي باستخدام JavaScript
+hide_button_script = """
+    <script>
+    document.addEventListener("DOMContentLoaded", function() {
+        var elements = document.getElementsByClassName('viewerBadge_container_r5tak');
+        if (elements.length > 0) {
+            elements[0].style.display = 'none';
+        }
+    });
+    </script>
 """
-st.markdown(hide_orange_button_style, unsafe_allow_html=True)
+st.markdown(hide_button_script, unsafe_allow_html=True)
